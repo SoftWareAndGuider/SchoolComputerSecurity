fdae484fea514c436ae29095f73dae4b891ec5f4
hj666c2 <hj666c3@gmail.com>
2019-12-06 13:29:51 +0900

    Made Installer

---
+++ SCS/SCSUInstaller/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SCSUInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaskList = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TaskList
+            // 
+            this.TaskList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TaskList.Enabled = false;
+            this.TaskList.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TaskList.FormattingEnabled = true;
+            this.TaskList.Items.AddRange(new object[] {
+            "설치 시작",
+            "디렉터리 생성",
+            "파일 받기",
+            "압축 해제",
+            "시작 프로그램 등록",
+            "설치 완료"});
+            this.TaskList.Location = new System.Drawing.Point(12, 12);
+            this.TaskList.Name = "TaskList";
+            this.TaskList.Size = new System.Drawing.Size(138, 120);
+            this.TaskList.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(156, 113);
+            this.progressBar1.MarqueeAnimationSpeed = 50;
+            this.progressBar1.Maximum = 5;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(290, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("돋움", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(156, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 95);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "설치 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(458, 151);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.TaskList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "SCSU Installer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox TaskList;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SCS/SCSUInstaller/Form1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCSUInstaller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TaskList.SetItemChecked(0, true);
+            try
+            {
+                Directory.Delete("C:\\SCS", true);
+                Directory.CreateDirectory("C:\\SCS");
+            }
+            catch
+            {
+                Directory.CreateDirectory("C:\\SCS");
+            }
+            TaskList.SetItemChecked(1, true);
+            progressBar1.Value = 1;
+
+            WebClient client = new WebClient();
+
+            string programpath = @"C:\SCS";
+            string ftpPath = "ftp://fileshareftp@nas.noeul.xyz/fileshare/SCS/SCSUUpdater.zip";
+            string targetPath = @"C:\SCS\SCSUUpdater.zip";
+            string userID = "fileshareftp";
+            string password = "noeulfile1412!";
+            client.Credentials = new NetworkCredential(userID, password);
+            client.DownloadFile(ftpPath, targetPath);
+            TaskList.SetItemChecked(2, true);
+            progressBar1.Value = 2;
+
+            ZipFile.ExtractToDirectory($@"{programpath}\SCSUUpdater.zip", programpath);
+            TaskList.SetItemChecked(3, true);
+            progressBar1.Value = 3;
+            var reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true); 
+            reg.SetValue("SCSU", @"C:\SCS\SCSUUpdater.exe");
+            TaskList.SetItemChecked(4, true);
+            progressBar1.Value = 4;
+            TaskList.SetItemChecked(5, true);
+            progressBar1.Value = 5;
+        }
+    }
+}
